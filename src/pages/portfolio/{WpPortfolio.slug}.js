4723b3093af604c2cecbf213fa806707b481cecc
Mark Furrow <mark@orbitmedia.com>
2021-05-16 21:42:15 -0700

    New portfolio presentation and FIXES keyboard listener portfolio navigation

--- src/pages/portfolio/{WpPortfolio.slug}.js
+++ src/pages/portfolio/{WpPortfolio.slug}.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useCallback } from "react"
 import { graphql } from "gatsby"
 
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
@@ -20,8 +20,8 @@ query PorfolioQuery($id: String!) {
         thumbnail: localFile {
           childImageSharp {
             gatsbyImageData(
-              width: 600
-              height: 600
+              width: 60
+              height: 60
               placeholder: BLURRED
               formats: [AUTO, WEBP, AVIF]
             )
@@ -49,33 +49,18 @@ query PorfolioQuery($id: String!) {
  * @param {*} param0 
  * @returns 
  */
-const PortfolioPhoto = ({ id, index, thumbnail, featured, featuredRow, title, caption, active, handlePresentation }) => {
+const PortfolioPhoto = ({ id, index, thumbnail, title, active, handlePresentation }) => {
 
   const thumbnailImage = getImage(thumbnail)
-  const featuredImage = getImage(featured)
-  const imageClass = active ? 'portfolioItem--active' : '';
-  const featuredStyle = {
-    gridRowStart: featuredRow
-  }
+  const imageClass = active ? 'portfolioImage--active' : '';
 
   return (
-    <>
-      <div className={`portfolioItem ${imageClass}`}
-        onClick={(e) => handlePresentation(e, index, id)}
-      >
-        <GatsbyImage
-          className={`portfolioImage`}
-          image={thumbnailImage} 
-          alt={title}
-        />
-        { active && <div className="portfolioItem__overlay"><h5>{title}</h5><div dangerouslySetInnerHTML={{__html: caption}}/></div> }
-      </div>
-      { active && (
-        <div className={`portfolioItem portfolioItem--featured`} style={featuredStyle}>
-          <GatsbyImage image={featuredImage} alt={title} />
-        </div>
-      ) }
-    </>
+    <GatsbyImage
+      className={`portfolioImage ${imageClass}`}
+      image={thumbnailImage} 
+      alt={title}
+      onClick={(e) => handlePresentation(e, index, id)}
+    />
   )
 
 }
@@ -84,36 +69,85 @@ const Portfolio = ({data}) => {
 
   const { title, excerpt, featuredImages } = data?.wpPortfolio
   // Set portfolioImages.
-  const [ portfolioImages, setPortfolioImages ] = useState( featuredImages?.images && featuredImages.images.map( image => (
-    {...image, active: false}
-  )))
+  const [ portfolioImages, setPortfolioImages ] = useState( featuredImages?.images && featuredImages.images.map( (image, index) => {
+    // Set the first item to active and rest not.
+    const active = index === 0 ? true : false
+    return {...image, active}
+  }))
+
+  // Manage current item index.
+  const [ currentIndex, setCurrentIndex ] = useState(0)
+
+  // First portfolio item becomes the featured image.
+  const [ featuredImage, setFeaturedImage ] = useState( portfolioImages[0] )
+
+  const handleKeyPress = useCallback( event => {
+
+    // Key pushed.
+    const { key } = event
+    const escKeys = ['ArrowRight', 'ArrowLeft']
+
+    // If arrowing left or right, let's continiue.
+    if (escKeys.includes(String(key))) {
+      
+      let nextIndex = null;
+      
+      // Move forward
+      if (key == 'ArrowRight' && currentIndex < portfolioImages.length) {
+          nextIndex = currentIndex + 1
+      }
+      
+      // Navigate back
+      if (key == 'ArrowLeft' && currentIndex > 1) {
+        nextIndex = currentIndex - 1
+      }
+
+      console.log(currentIndex, nextIndex)
+
+      const updatedPortfolioImages = portfolioImages.map( (image, index) => {
+        if (index === nextIndex) { 
+          setFeaturedImage(image)
+          setCurrentIndex(nextIndex)
+          return Object.assign({}, {...image}, {active: true})
+        } else {
+          return Object.assign({}, {...image}, {active: false})
+        }
+      })
+      
+      // Update portfolioImages state.
+      setPortfolioImages(updatedPortfolioImages)
+      
+    }
+
+  }, [currentIndex])
+
+  useEffect(() => {
+    
+    window.addEventListener( 'keydown', handleKeyPress )
+
+    return () => {
+      window.removeEventListener( 'keydown', handleKeyPress )
+    }
+
+  })
 
   const handlePresentation = (e, index, id) => {
 
-    const imageHeight = e.target.offsetHeight
-    const elementOffset = e.target.getBoundingClientRect().top
-    const y = elementOffset + window.pageYOffset + imageHeight
-    window.scrollTo({top: y, behavior: 'smooth'})
-    // console.log(e.target.offsetHeight)
-    // console.log(e.target)
-    // e.target.scrollIntoView()
     const order = index + 1
+
+    setCurrentIndex(index)
+
+    console.log(currentIndex)
 
     // Set clicked image to active and every other to not.
     const updatedPortfolioImages = portfolioImages.map( (image, index) => {
       
       if (image.id === id) {
-        // If clicked image is currently active then deactivate, otherwise set to active.
-        const active = image.active ? false : true
-        // const featuredOrder = (index % 3)
-        // order = order + (index % 3) + 10
-        // console.log(order)
-        const featuredRow = Math.ceil(order/3) + 1
-        console.log(index, featuredRow)
-        return Object.assign({}, {...image}, {active, order, featuredRow})
+        setFeaturedImage(image)
+        return Object.assign({}, {...image}, {active: true})
       }
       
-      return Object.assign({}, {...image}, {active: false, order})
+      return Object.assign({}, {...image}, {active: false})
     })
 
     // Update portfolioImages state.
@@ -127,9 +161,14 @@ const Portfolio = ({data}) => {
       <h1>{ title }</h1>
       <div dangerouslySetInnerHTML={{__html: excerpt}} />
       <div className="portfolio">
-      { portfolioImages &&
-        portfolioImages.map( (image, index ) => <PortfolioPhoto key={image.id} index={index} featuredRow={image.featuredRow} {...image} handlePresentation={handlePresentation} /> )
-      }
+        <div className="portfolio__featured">
+          { featuredImage && <GatsbyImage image={getImage(featuredImage.featured)} alt={featuredImage.title} /> }
+        </div>
+        <div className="portfolio__grid">
+          { portfolioImages &&
+            portfolioImages.map( (image, index ) => <PortfolioPhoto key={image.id} index={index} featuredRow={image.featuredRow} {...image} handlePresentation={handlePresentation} /> )
+          }
+        </div>
       </div>
     </Layout>
   )
