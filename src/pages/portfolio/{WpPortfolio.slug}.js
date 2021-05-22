cf58db715e623bb4018d557d3986ff86390ea62a
Mark Furrow <mark@orbitmedia.com>
2021-05-22 08:57:59 -0700

    Simplified portfolio code w/ arrow nav

--- src/pages/portfolio/{WpPortfolio.slug}.js
+++ src/pages/portfolio/{WpPortfolio.slug}.js
@@ -30,7 +30,8 @@ query PorfolioQuery($id: String!) {
         featured: localFile {
           childImageSharp {
             gatsbyImageData(
-              layout: FULL_WIDTH
+              layout: CONSTRAINED
+              height: 700
               placeholder: BLURRED
               formats: [AUTO, WEBP, AVIF]
             )
@@ -41,6 +42,19 @@ query PorfolioQuery($id: String!) {
   }
 }
 `
+
+const PortfolioCount = ({current, total}) => (
+  <div className="porfolio__count">{current+1}/{total}</div>
+)
+
+const PortfolioPhotoDetail = ({title, caption}) => {
+  return (
+    <>
+      <h4>{ title }</h4>
+      <div dangerouslySetInnerHTML={{__html: caption}}/>
+    </>
+  )
+}
 
 /**
  * @todo Explore grid re-ordering based on index
@@ -67,59 +81,42 @@ const PortfolioPhoto = ({ id, index, thumbnail, title, active, handlePresentatio
 
 const Portfolio = ({data}) => {
 
-  const { title, excerpt, featuredImages } = data?.wpPortfolio
+  const { title, excerpt, featuredImages } = data?.wpPortfolio || []
   // Set portfolioImages.
   const [ portfolioImages, setPortfolioImages ] = useState( featuredImages?.images && featuredImages.images.map( (image, index) => {
     // Set the first item to active and rest not.
     const active = index === 0 ? true : false
-    return {...image, active}
+    return {...image, active: index === 0}
   }))
 
   // Manage current item index.
   const [ currentIndex, setCurrentIndex ] = useState(0)
 
+  const featuredPortfolioImage = (portfolioImages && portfolioImages.length) ? portfolioImages[0] : []
+
   // First portfolio item becomes the featured image.
-  const [ featuredImage, setFeaturedImage ] = useState( portfolioImages[0] )
+  const [ featuredImage, setFeaturedImage ] = useState( featuredPortfolioImage )
 
   const handleKeyPress = useCallback( event => {
 
     // Key pushed.
     const { key } = event
-    const escKeys = ['ArrowRight', 'ArrowLeft']
+    const escKeys = [ 'ArrowRight', 'ArrowLeft' ]
 
     // If arrowing left or right, let's continiue.
     if (escKeys.includes(String(key))) {
       
-      let nextIndex = null;
-      
       // Move forward
-      if (key == 'ArrowRight' && currentIndex < portfolioImages.length) {
-          nextIndex = currentIndex + 1
+      if (key == 'ArrowRight') {
+        return goToNextImage()
       }
       
       // Navigate back
-      if (key == 'ArrowLeft' && currentIndex > 1) {
-        nextIndex = currentIndex - 1
-      }
-
-      console.log(currentIndex, nextIndex)
-
-      const updatedPortfolioImages = portfolioImages.map( (image, index) => {
-        if (index === nextIndex) { 
-          setFeaturedImage(image)
-          setCurrentIndex(nextIndex)
-          return Object.assign({}, {...image}, {active: true})
-        } else {
-          return Object.assign({}, {...image}, {active: false})
-        }
-      })
-      
-      // Update portfolioImages state.
-      setPortfolioImages(updatedPortfolioImages)
+      return goToPreviousImage()
       
     }
-
-  }, [currentIndex])
+    
+  })
 
   useEffect(() => {
     
@@ -129,31 +126,35 @@ const Portfolio = ({data}) => {
       window.removeEventListener( 'keydown', handleKeyPress )
     }
 
-  })
+  }, [currentIndex, featuredImage, portfolioImages])
 
-  const handlePresentation = (e, index, id) => {
+  const goToNextImage = () => {
+    let nextIndex = currentIndex
+    if (currentIndex < portfolioImages.length) {
+      nextIndex++
+      setFeaturedImageByIndex(nextIndex)
+      setCurrentIndex(nextIndex)
+    }
+  }
 
-    const order = index + 1
+  const goToPreviousImage = () => {
+    let nextIndex = currentIndex
+    if (currentIndex >= 1) {
+      nextIndex--
+      setFeaturedImageByIndex(nextIndex)
+      setCurrentIndex(nextIndex)
+    }
+  }
 
+  const setFeaturedImageByIndex = (nextIndex) => {
+    const featuredImage = portfolioImages.filter( (image, index) => index === nextIndex )
+    if (featuredImage && featuredImage.length) setFeaturedImage(featuredImage[0])
+  }
+
+  const handleThumbnailClick = (e, index, id) => {
+    const featuredImage = portfolioImages.filter( (image) => image.id === id )
+    if (featuredImage && featuredImage.length) setFeaturedImage(featuredImage[0])
     setCurrentIndex(index)
-
-    console.log(currentIndex)
-
-    // Set clicked image to active and every other to not.
-    const updatedPortfolioImages = portfolioImages.map( (image, index) => {
-      
-      if (image.id === id) {
-        setFeaturedImage(image)
-        return Object.assign({}, {...image}, {active: true})
-      }
-      
-      return Object.assign({}, {...image}, {active: false})
-    })
-
-    // Update portfolioImages state.
-    setPortfolioImages(updatedPortfolioImages)
-
-    console.log(updatedPortfolioImages)
   }
 
   return (
@@ -164,11 +165,30 @@ const Portfolio = ({data}) => {
         <div className="portfolio__featured">
           { featuredImage && <GatsbyImage image={getImage(featuredImage.featured)} alt={featuredImage.title} /> }
         </div>
-        <div className="portfolio__grid">
-          { portfolioImages &&
-            portfolioImages.map( (image, index ) => <PortfolioPhoto key={image.id} index={index} featuredRow={image.featuredRow} {...image} handlePresentation={handlePresentation} /> )
+        <div className="portfolio__right">
+          {
+            portfolioImages && <PortfolioCount current={currentIndex} total={portfolioImages.length}/>
+          }
+          <div className="portfolio__grid">
+            { 
+              portfolioImages &&
+              portfolioImages.map( (image, index ) => {
+                return (
+                  <GatsbyImage 
+                    key={image.id}
+                    image={getImage(image.thumbnail)}
+                    alt={image.title}
+                    onClick={(e) => handleThumbnailClick(e, index, image.id)} 
+                  /> 
+                )
+              })
+            }
+          </div>
+          {
+            featuredImage && <PortfolioPhotoDetail {...featuredImage}/>
           }
         </div>
+        
       </div>
     </Layout>
   )
